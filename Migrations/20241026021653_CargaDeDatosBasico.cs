ddeb80d28901dd9c737af343edc9963b82bd9cb0
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2024-10-25 23:20:11 -0300

    comenzando a cargar datos en la DB

---
+++ Migrations/20241026021653_CargaDeDatosBasico.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace lion_force_be.Migrations
+{
+    /// <inheritdoc />
+    public partial class CargaDeDatosBasico : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Roles",
+                type: "varchar(15)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Degree",
+                table: "Belts",
+                type: "varchar(2)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeltRank",
+                table: "Belts",
+                type: "varchar(15)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Belts",
+                columns: new[] { "Id", "BeltRank", "Degree" },
+                values: new object[,]
+                {
+                    { 1, "Blanco", null },
+                    { 2, "Naranja", null },
+                    { 3, "Amarillo", null },
+                    { 4, "Camuflado", null },
+                    { 5, "Verde", null },
+                    { 6, "Violeta", null },
+                    { 7, "Azul", null },
+                    { 8, "Marron", null },
+                    { 9, "Rojo", null },
+                    { 10, "Rojo negro", null },
+                    { 11, "Negro", "1" },
+                    { 12, "Negro", "2" },
+                    { 13, "Negro", "3" },
+                    { 14, "Negro", "4" },
+                    { 15, "Negro", "5" },
+                    { 16, "Negro", "6" },
+                    { 17, "Negro", "7" },
+                    { 18, "Negro", "8" },
+                    { 19, "Negro", "9" },
+                    { 20, "Negro", "10" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Admin" },
+                    { 2, "Supervisor" },
+                    { 3, "Instructor" },
+                    { 4, "Student" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Belts",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Roles",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Degree",
+                table: "Belts",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(2)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BeltRank",
+                table: "Belts",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- Shared/DbContext.cs
+++ Shared/DbContext.cs
@@ -1,4 +1,3 @@
-
 using lion_force_be.Models;
 using lion_force_be.Models.Relations;
 using Microsoft.EntityFrameworkCore;
@@ -23,6 +22,48 @@ public sealed class DbContextLF : DbContext
       tb.HasKey(us => new { us.UserId, us.ServiceId, us.PaymentDate });
       tb.HasOne(us => us.User).WithMany(u => u.UserServices).HasForeignKey(us => us.UserId);
       tb.HasOne(us => us.Service).WithMany(s => s.UserServices).HasForeignKey(us => us.ServiceId);
+    });
+    builder.Entity<Role>(tb =>
+    {
+      tb.Property(r => r.Name).HasColumnType("varchar(15)");
+      tb.HasData(
+        new Role { Id = 1, Name = "Admin" },
+        new Role { Id = 2, Name = "Supervisor" },
+        new Role { Id = 3, Name = "Instructor" },
+        new Role { Id = 4, Name = "Student" }
+      );
+    });
+    builder.Entity<Belt>(tb =>
+    {
+      tb.Property(b => b.BeltRank).HasColumnType("varchar(15)");
+      tb.Property(b => b.Degree).HasColumnType("varchar(2)");
+      tb.HasData(
+        new Belt { Id = 1, BeltRank = "Blanco" },
+        new Belt { Id = 2, BeltRank = "Naranja" },
+        new Belt { Id = 3, BeltRank = "Amarillo" },
+        new Belt { Id = 4, BeltRank = "Camuflado" },
+        new Belt { Id = 5, BeltRank = "Verde" },
+        new Belt { Id = 6, BeltRank = "Violeta" },
+        new Belt { Id = 7, BeltRank = "Azul" },
+        new Belt { Id = 8, BeltRank = "Marron" },
+        new Belt { Id = 9, BeltRank = "Rojo" },
+        new Belt { Id = 10, BeltRank = "Rojo negro" },
+        new Belt { Id = 11, BeltRank = "Negro", Degree = "1" },
+        new Belt { Id = 12, BeltRank = "Negro", Degree = "2" },
+        new Belt { Id = 13, BeltRank = "Negro", Degree = "3" },
+        new Belt { Id = 14, BeltRank = "Negro", Degree = "4" },
+        new Belt { Id = 15, BeltRank = "Negro", Degree = "5" },
+        new Belt { Id = 16, BeltRank = "Negro", Degree = "6" },
+        new Belt { Id = 17, BeltRank = "Negro", Degree = "7" },
+        new Belt { Id = 18, BeltRank = "Negro", Degree = "8" },
+        new Belt { Id = 19, BeltRank = "Negro", Degree = "9" },
+        new Belt { Id = 20, BeltRank = "Negro", Degree = "10" }
+      );
+    });
+    builder.Entity<User>(tb =>
+    {
+      tb.Property(u => u.Name).HasColumnType("varchar(20)");
+      tb.Property(u => u.LastName).HasColumnType("varchar(20)");
     });
   }
 }
