04f86cfdeb68afacf154480d7f30ba7c697b1a0b
Gonzalo Mansilla <gonzalo2nm@gmail.com>
2024-11-28 08:13:25 -0300

    listados de servicios para  permisos de supervisor y admin, y listado de servicios por academia

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -1,9 +1,11 @@
 
+using System.Security.Claims;
 using AutoMapper;
 using lion_force_be.DTOs;
 using lion_force_be.Models;
 using lion_force_be.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lion_force_be.Controllers;
@@ -20,8 +22,7 @@ public class ServiceController(ServiceService service, IMapper mapper) : Control
   [HttpPost]
   public async Task<ActionResult<ResponseOne<ServiceDTO>>> Add(ServiceRequestDTO serviceRDTO)
   {
-    var res = new ResponseOne<ServiceDTO> { Status = "", Message = "", Data = null };
-    res = await _service.Add(serviceRDTO);
+    var res = await _service.Add(serviceRDTO);
     switch (res.Status)
     {
       case "201":
@@ -33,33 +34,66 @@ public class ServiceController(ServiceService service, IMapper mapper) : Control
     }
   }
 
+
+  [Authorize(Policy = "ControlRoles")]
+  [HttpGet]
+  public async Task<ActionResult<ResponseList<ServiceDTO>>> GetAll()
+  {
+    ResponseList<Service> serviceRes = await _service.GetAll();
+    var servicesDTO = _mapper.Map<List<ServiceDTO>>(serviceRes);
+    var res = new ResponseList<ServiceDTO> { Status = serviceRes.Status, Message = serviceRes.Message, Data = servicesDTO };
+    switch (res.Status)
+    {
+      case "200": return Ok(res);
+      default: return StatusCode(StatusCodes.Status500InternalServerError, res);
+    }
+  }
+
+  [Authorize(Policy = "NotStudent")]
+  [HttpGet("{academyid}")]
+  public async Task<ActionResult<ResponseList<ServiceDTO>>> GetAllByAcademy(int academyid)
+  {
+    bool ControlRole = false;
+    var res = new ResponseList<ServiceDTO> { Status = "", Message = "", Data = [] };
+    var userRole = User.FindFirst(ClaimTypes.Role)?.Value;
+    if (userRole == null)
+    {
+      res.UpdateValues("400", "No se identifico su authorizacion", []);
+      return BadRequest(res);
+    }
+    var userDNI = User.FindFirst("dni")?.Value;
+    if (userDNI == null)
+    {
+      res.UpdateValues("400", "No se pudo identificar al usuario", []);
+      return BadRequest(res);
+    }
+    if (userRole.Equals("Admin", StringComparison.OrdinalIgnoreCase) || userRole.Equals("Supervisor", StringComparison.OrdinalIgnoreCase))
+    {
+      ControlRole = true;
+    }
+    var serviceRes = await _service.GetAllByAcademy(userDNI, academyid, ControlRole);
+    var servicesDTO = _mapper.Map<List<ServiceDTO>>(serviceRes);
+    res.UpdateValues(serviceRes.Status, serviceRes.Message, servicesDTO);
+    switch (res.Status)
+    {
+      case "200": return Ok(res);
+      case "400": return BadRequest(res);
+      default: return StatusCode(StatusCodes.Status500InternalServerError, res);
+    }
+  }
   /*
-    [Authorize(Policy = "ControlRoles")]
-    [HttpGet]
-    public async Task<ActionResult<ResponseList<ServiceDTO>>> GetAll()
-    {
-      return await _service.GetAll();
-    }
+        [Authorize(Policy = "NotStudent")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ResponseOne<ServiceDTO>>> Update()
+        {
+          return await _service.Update();
+        }
 
-    [Authorize(Policy = "NotStudent")]
-    [HttpGet("{academyid}")]
-    public async Task<ActionResult<ResponseList<ServiceDTO>>> GetAllByAcademy()
-    {
-      return await _service.GetAllByAcademy();
-    }
-
-    [Authorize(Policy = "NotStudent")]
-    [HttpPut("{id}")]
-    public async Task<ActionResult<ResponseOne<ServiceDTO>>> Update()
-    {
-      return await _service.Update();
-    }
-
-    [Authorize(Policy = "NotStudent")]
-    [HttpDelete("{id}")]
-    public async Task<ActionResult<ResponseOne<ServiceDTO>>> Delete()
-    {
-      return await _service.Delete();
-    }
-  */
+        [Authorize(Policy = "NotStudent")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ResponseOne<ServiceDTO>>> Delete()
+        {
+          return await _service.Delete();
+        }
+      */
 }
--- Services/ServiceService.cs
+++ Services/ServiceService.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using lion_force_be.DBContext;
 using lion_force_be.DTOs;
 using lion_force_be.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace lion_force_be.Services;
 
@@ -38,17 +39,55 @@ public class ServiceService(DbContextLF dbContext, IMapper mapper)
       return res;
     }
   }
+
+  public async Task<ResponseList<Service>> GetAll()
+  {
+    var res = new ResponseList<Service> { Status = "", Message = "", Data = [] };
+    try
+    {
+      var services = await _dbContext.Services.ToListAsync();
+      res.UpdateValues("200", "Listado de Servicios", services);
+      return res;
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.Message);
+      res.UpdateValues("500", "Ocurrio un error al listar todos los servicios", []);
+      return res;
+    }
+  }
+
+  public async Task<ResponseList<Service>> GetAllByAcademy(string instructorDNI, int academyid, bool ControleRole)
+  {
+    var res = new ResponseList<Service> { Status = "", Message = "", Data = [] };
+    List<Service> services;
+    try
+    {
+      if (ControleRole)
+      {
+        services = await _dbContext.Services.Where(s => s.AcademyId == academyid).ToListAsync();
+      }
+      else
+      {
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.DNI == instructorDNI && u.AcademyId == academyid);
+        if (user == null)
+        {
+          res.UpdateValues("400", "No puede pedir servicios que sean de otra academia", []);
+          return res;
+        }
+        services = await _dbContext.Services.Where(s => s.AcademyId == academyid).ToListAsync();
+      }
+      res.UpdateValues("200", "Listado de servicios de su academia", services);
+      return res;
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.Message);
+      res.UpdateValues("500", "Ocurrio un error al buscar los servicios de su academia", []);
+      return res;
+    }
+  }
   /*
-    public async Task<ResponseList<Service>> GetAll()
-    {
-      return await true;
-    }
-
-    public async Task<ResponseList<Service>> GetAllByAcademy()
-    {
-      return await true;
-    }
-
     public async Task<ResponseOne<Service>> Update()
     {
       return await true;
